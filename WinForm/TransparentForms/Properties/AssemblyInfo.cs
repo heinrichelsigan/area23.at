d4b24833a2454b0ee0790506291bd5360f24a0aa
Heinrich Elsigan <heinrich.elsigan@gmaiil.com>
2024-12-17 05:30:04 +0100

    2024-12-17 updated version numbers

--- WinForm/TransparentForms/Properties/AssemblyInfo.cs
+++ WinForm/TransparentForms/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.24.1105.0")]
-[assembly: AssemblyFileVersion("2.24.1105.0")]
+[assembly: AssemblyVersion("2.24.1226.0")]
+[assembly: AssemblyFileVersion("2.24.1226.0")]
