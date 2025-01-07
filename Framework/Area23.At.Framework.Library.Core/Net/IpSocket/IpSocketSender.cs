dbbc9cf098ab9dbdedeb1bee4bf6cbee104af47c
Heinrich Elsigan <heinrich.elsigan@gmail.com>
2025-01-07 17:42:05 +0100

    2025-01-07 ip socket send try 4

--- Framework/Area23.At.Framework.Library.Core/Net/IpSocket/IpSocketSender.cs
+++ Framework/Area23.At.Framework.Library.Core/Net/IpSocket/IpSocketSender.cs
@@ -10,23 +10,7 @@ using Microsoft.Win32;
 namespace Area23.At.Framework.Library.Core.Net.IpSocket
 {
     public static class IPSocketSender
-    {
-        const string TEST_HTTP_REQUEST_HEADER = @"GET / HTTP/1.1
-Host: heinrichelsigan.area23.at
-User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-Accept-Language: en-US,en;q=0.5
-Accept-Encoding: gzip, deflate, br, zstd
-Connection: keep-alive
-Upgrade-Insecure-Requests: 1
-Sec-Fetch-Dest: document
-Sec-Fetch-Mode: navigate
-Sec-Fetch-Site: same-origin
-If-Modified-Since: Mon, 02 Dec 2024 04:17:07 GMT
-If-None-Match: ""1c3e-62841d2582843-gzip""
-Priority: u=0, i
-Pragma: no-cache
-Cache-Control: no-cache";
+    {       
 
         /// <summary>
         /// Send
@@ -41,13 +25,13 @@ Cache-Control: no-cache";
             {
                 IPEndPoint serverIep = new IPEndPoint(serverIp, serverPort);
                 TcpClient tcpClient = new TcpClient();
-                byte[] data = Encoding.ASCII.GetBytes(TEST_HTTP_REQUEST_HEADER);
+                byte[] data = Encoding.UTF8.GetBytes(msg);
                 tcpClient.Connect(serverIep);
                 // tcpClient.Client.Send(data);
                 NetworkStream netStream = tcpClient.GetStream();
                 StreamWriter sw = new StreamWriter(netStream);
                 // StreamReader sr = new StreamReader(netStream);
-                sw.Write(TEST_HTTP_REQUEST_HEADER);
+                sw.Write(msg);
                 sw.Flush();
                 // byte[] outbuf = new byte[8192];
                 // int read = tcpClient.Client.Receive(outbuf);
