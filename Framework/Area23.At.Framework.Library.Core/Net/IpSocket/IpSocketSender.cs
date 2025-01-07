6d34bd49b2fa1b4ce9697b78158101f848b61842
Heinrich Elsigan <heinrich.elsigan@gmail.com>
2025-01-07 17:08:55 +0100

    2025-01-07 IpSocket Sender Try2

--- Framework/Area23.At.Framework.Library.Core/Net/IpSocket/IpSocketSender.cs
+++ Framework/Area23.At.Framework.Library.Core/Net/IpSocket/IpSocketSender.cs
@@ -7,32 +7,47 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Win32;
 
-namespace Area23.At.Framework.Library.Core.Net.WebHttp
+namespace Area23.At.Framework.Library.Core.Net.IpSocket
 {
-    public static class IpSocketSender
+    public static class IPSocketSender
     {
+        const string TEST_HTTP_REQUEST_HEADER = @"GET / HTTP/1.1
+Host: heinrichelsigan.area23.at
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate, br, zstd
+Connection: keep-alive
+Upgrade-Insecure-Requests: 1
+Sec-Fetch-Dest: document
+Sec-Fetch-Mode: navigate
+Sec-Fetch-Site: same-origin
+If-Modified-Since: Mon, 02 Dec 2024 04:17:07 GMT
+If-None-Match: ""1c3e-62841d2582843-gzip""
+Priority: u=0, i
+Pragma: no-cache
+Cache-Control: no-cache";
 
         /// <summary>
-        /// IpSocketSend
+        /// Send
         /// </summary>
         /// <param name="serverIp">server ip address</param>
-        /// <param name="msg"></param>
         /// <param name="serverPort">server port (default 7777)</param>
         /// <returns>client address as string</returns>
-        public static string IpSocketSend(IPAddress serverIp, string msg, int serverPort = 7777)
+        public static string Send(IPAddress serverIp, string msg, int serverPort = 7777)
         {
             string? resp = string.Empty;
             try
             {
                 IPEndPoint serverIep = new IPEndPoint(serverIp, serverPort);
                 TcpClient tcpClient = new TcpClient();
-                byte[] data = Encoding.ASCII.GetBytes(msg);
+                byte[] data = Encoding.ASCII.GetBytes(TEST_HTTP_REQUEST_HEADER);
                 tcpClient.Connect(serverIep);
                 // tcpClient.Client.Send(data);
                 NetworkStream netStream = tcpClient.GetStream();
                 StreamWriter sw = new StreamWriter(netStream);
                 // StreamReader sr = new StreamReader(netStream);
-                sw.Write(msg);
+                sw.Write(TEST_HTTP_REQUEST_HEADER);
                 sw.Flush();
                 // byte[] outbuf = new byte[8192];
                 // int read = tcpClient.Client.Receive(outbuf);
